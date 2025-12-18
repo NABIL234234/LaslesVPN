bc022204c636807a5f70942a5b2ba015b063ad66
Nabil <sarzanovnabil108@gmail.com>
2025-12-18 11:39:56 +0600

    ''Completed new sections Pricing and Global, else added new component plan"

--- src/pages/Home/Features/Feature.jsx
+++ src/pages/Home/Features/Feature.jsx
@@ -5,36 +5,38 @@ import check from "../../../assets/check.png";
 function Feature() {
   return (
     <>
-      <div className="feature-wrapper">
-        <div>
-          <img src={illustration} alt="illustration" />
-        </div>
-        <div className="feature-container">
-          <h2>We Provide Many Features You Can Use</h2>
-          <p>
-            You can explore the features that we provide with fun and have their
-            own functions each feature.
-          </p>
-          <div className="check-container">
-            <div className="check-wrapper">
-              <img src={check} alt="check" />
-              <span>Powerfull online protection.</span>
-            </div>
-            <div className="check-wrapper">
-              <img src={check} alt="check" />
-              <span>Internet without borders.</span>
-            </div>
-            <div className="check-wrapper">
-              <img src={check} alt="check" />
-              <span>Supercharged VPN</span>
-            </div>
-            <div className="check-wrapper">
-              <img src={check} alt="check" />
-              <span>No specific time limits.</span>
+      <section>
+        <div className="feature-wrapper">
+          <div>
+            <img src={illustration} alt="illustration" />
+          </div>
+          <div className="feature-container">
+            <h2>We Provide Many Features You Can Use</h2>
+            <p>
+              You can explore the features that we provide with fun and have
+              their own functions each feature.
+            </p>
+            <div className="check-container">
+              <div className="check-wrapper">
+                <img src={check} alt="check" />
+                <span>Powerfull online protection.</span>
+              </div>
+              <div className="check-wrapper">
+                <img src={check} alt="check" />
+                <span>Internet without borders.</span>
+              </div>
+              <div className="check-wrapper">
+                <img src={check} alt="check" />
+                <span>Supercharged VPN</span>
+              </div>
+              <div className="check-wrapper">
+                <img src={check} alt="check" />
+                <span>No specific time limits.</span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
---
+++ src/pages/Home/Pricing/Pricing.jsx
@@ -0,0 +1,68 @@
+import "./Pricing.css";
+
+import Plan from "../../../components/Plan/Plan";
+
+function Pricing() {
+  const plansData = [
+    {
+      id: 1,
+      title: "Free Plan",
+      price: "Free",
+      features: [
+        "Unlimited Bandwidth",
+        "Encrypted Connection",
+        "No Traffic Logs",
+        "Works on All Devices",
+      ],
+      isPopular: false,
+    },
+    {
+      id: 2,
+      title: "Standard Plan",
+      price: "$9 / mo",
+      features: [
+        "Unlimited Bandwidth",
+        "Encrypted Connection",
+        "No Traffic Logs",
+        "Works on All Devices",
+        "Connect Anyware",
+      ],
+      isPopular: true,
+    },
+    {
+      id: 3,
+      title: "Premium Plan",
+      price: "$12 / mo",
+      features: [
+        "Unlimited Bandwidth",
+        "Encrypted Connection",
+        "No Traffic Logs",
+        "Works on All Devices",
+        "Connect Anyware",
+        "Get New Features",
+      ],
+      isPopular: false,
+    },
+  ];
+
+  return (
+    <>
+      <section>
+        <div className="plan-table">
+          <h1>Choose Your Plan</h1>
+          <p>
+            Let's choose the package that is best for you and explore it happily
+            and <br/> cheerfully.
+          </p>
+        </div>
+        <div className="plan-wrapper">
+          {plansData.map((plan) => (
+            <Plan key={plan.id} infoPlan={plan} />
+          ))}
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Pricing;
